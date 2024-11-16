0342dfa02a39cfd865710123c0e7e6e6fba7e133
Milla Kilpeläinen <kilpelainenmilla@outlook.com>
2024-11-16 20:55:05 +0200

    tehtäviä

--- 10/10.2.py
+++ 10/10.2.py
@@ -1,4 +1,4 @@
-# moduuli 10, tehtävä 1
+# moduuli 10, tehtävä 2
 
 class Hissi:
     def __init__(self, nimi, kerros=1, alin_kerros=1, ylin_kerros=10):
@@ -24,6 +24,17 @@ class Hissi:
         print(f'{self.nimi} on nyt kerroksessa {self.kerros}.')
         return
 
-h1 = Hissi('h1')
-h1.siirry_kerrokseen(5)
-h1.siirry_kerrokseen(1)
+# --------------------------------------------------TALO----------------------------------------------------
+
+class Talo:
+    def __init__(self, hissit, alin_kerros=1, ylin_kerros=10):
+        self.hissit = hissit
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
+
+    def aja_hissiä(self, hissin_numero, kohdekerros):
+        self.hissit[hissin_numero].siirry_kerrokseen(kohdekerros)
+
+t1 = Talo([Hissi("h1"), Hissi("h2")])
+t1.aja_hissiä(0, 6)
+print(f'{t1.hissit[0].nimi} on kerroksessa {t1.hissit[0].kerros}.')
